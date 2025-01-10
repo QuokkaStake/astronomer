4b6b241620e5e777728acef963a21725ab6f38a0
Sergey <freak12techno@gmail.com>
2025-01-10 22:22:53 +0300

    chore: add /chains test

---
+++ pkg/interacter/telegram/chains_test.go
@@ -0,0 +1,209 @@
+package telegram
+
+import (
+	"errors"
+	"main/assets"
+	databasePkg "main/pkg/database"
+	loggerPkg "main/pkg/logger"
+	"main/pkg/metrics"
+	"main/pkg/types"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/require"
+	tele "gopkg.in/telebot.v3"
+)
+
+//nolint:paralleltest // disabled
+func TestTelegramChainsErrorFetchingChains(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("Error fetching chains!"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}).AddRow("chain1").AddRow("chain2"))
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		nil,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/chains",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/chains", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramChainsErrorFetchingExplorers(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("Error fetching explorers!"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}).AddRow("chain1").AddRow("chain2"))
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnRows(sqlmock.
+			NewRows([]string{"name", "pretty_name", "base_denom", "bech32_validator_prefix"}).
+			AddRow("chain", "Chain", "ustake", "chainvaloper"),
+		)
+
+	mock.ExpectQuery("SELECT chain, name, proposal_link_pattern, wallet_link_pattern, validator_link_pattern, main_link FROM explorers").
+		WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		nil,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/chains",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/chains", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramChainsOk(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasBytes(assets.GetBytesOrPanic("responses/chains.html")),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}).AddRow("chain1").AddRow("chain2"))
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnRows(sqlmock.
+			NewRows([]string{"name", "pretty_name", "base_denom", "bech32_validator_prefix"}).
+			AddRow("chain", "Chain", "ustake", "chainvaloper"),
+		)
+
+	mock.ExpectQuery("SELECT chain, name, proposal_link_pattern, wallet_link_pattern, validator_link_pattern, main_link FROM explorers").
+		WillReturnRows(sqlmock.
+			NewRows([]string{"chain", "name", "proposal_link_pattern", "wallet_link_pattern", "validator_link_pattern", "main_link"}).
+			AddRow(
+				"chain",
+				"Ping",
+				"",
+				"",
+				"",
+				"https://example.com",
+			),
+		)
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		nil,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/chains",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/chains", ctx)
+	require.NoError(t, err)
+}
