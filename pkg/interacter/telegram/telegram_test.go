d1aa15e3c560b6639f510385efc89e7c81c8235d
Sergey <freak12techno@gmail.com>
2025-01-10 19:26:08 +0300

    chore: add tests for db fails

--- pkg/interacter/telegram/telegram_test.go
+++ pkg/interacter/telegram/telegram_test.go
@@ -3,10 +3,14 @@ package telegram
 import (
 	"errors"
 	"main/assets"
+	databasePkg "main/pkg/database"
 	loggerPkg "main/pkg/logger"
+	"main/pkg/metrics"
 	"main/pkg/types"
 	"strings"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
 
 	"github.com/stretchr/testify/require"
 	tele "gopkg.in/telebot.v3"
@@ -158,5 +162,109 @@ func TestTelegramSendMultilineOk(t *testing.T) {
 	})
 
 	err := interacter.BotReply(ctx, strings.Repeat("a", 5000))
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramAddCommandFailedToInsertQuery(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("Internal error!"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		nil,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/help",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/help", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramAddCommandFailedToFetchChains(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("Internal error!"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		nil,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/help",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/help", ctx)
 	require.NoError(t, err)
 }
