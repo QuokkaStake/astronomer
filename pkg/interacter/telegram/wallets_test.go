9b3b45eb77c145179d5760c0cd262dc033680e31
Sergey <freak12techno@gmail.com>
2025-01-11 03:54:45 +0300

    chore: add /wallets test

---
+++ pkg/interacter/telegram/wallets_test.go
@@ -0,0 +1,344 @@
+package telegram
+
+import (
+	"errors"
+	"main/assets"
+	datafetcher "main/pkg/data_fetcher"
+	databasePkg "main/pkg/database"
+	loggerPkg "main/pkg/logger"
+	"main/pkg/metrics"
+	"main/pkg/types"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/require"
+	tele "gopkg.in/telebot.v3"
+)
+
+//nolint:paralleltest // disabled
+func TestTelegramWalletsErrorFetchingChainBinds(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("❌ Error getting wallets list: custom error"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+	dataFetcher := datafetcher.NewDataFetcher(logger, database, nil, metricsManager, nil)
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}).AddRow("chain1").AddRow("chain2"))
+
+	mock.ExpectQuery("SELECT chain, reporter, user_id, address, alias FROM wallet_links").
+		WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		dataFetcher,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/wallets",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/wallets", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramWalletsErrorFetchingChains(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("❌ Error getting wallets list: custom error"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+	dataFetcher := datafetcher.NewDataFetcher(logger, database, nil, metricsManager, nil)
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}).AddRow("chain1").AddRow("chain2"))
+
+	mock.ExpectQuery("SELECT chain, reporter, user_id, address, alias FROM wallet_links").
+		WillReturnRows(sqlmock.NewRows([]string{"chain", "reporter", "user_id", "address", "alias"}).
+			AddRow("chain1", "reporter1", 1, "address1", "alias1"))
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		dataFetcher,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/wallets",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/wallets", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramWalletsErrorFetchingExplorers(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("❌ Error getting wallets list: custom error"),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+	dataFetcher := datafetcher.NewDataFetcher(logger, database, nil, metricsManager, nil)
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}).AddRow("chain1").AddRow("chain2"))
+
+	mock.ExpectQuery("SELECT chain, reporter, user_id, address, alias FROM wallet_links").
+		WillReturnRows(sqlmock.NewRows([]string{"chain", "reporter", "user_id", "address", "alias"}).
+			AddRow("chain1", "reporter1", 1, "address1", "alias1"))
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnRows(sqlmock.
+			NewRows([]string{"name", "pretty_name", "base_denom", "bech32_validator_prefix"}).
+			AddRow("chain1", "Chain 1", "uatom", "cosmosvaloper"),
+		)
+
+	mock.ExpectQuery("SELECT chain, name, proposal_link_pattern, wallet_link_pattern, validator_link_pattern, main_link FROM explorers").
+		WillReturnError(errors.New("custom error"))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		dataFetcher,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/wallets",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/wallets", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramWalletsErrorNoChains(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasText("You are not subscribed to any wallets."),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+	dataFetcher := datafetcher.NewDataFetcher(logger, database, nil, metricsManager, nil)
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}))
+
+	mock.ExpectQuery("SELECT chain, reporter, user_id, address, alias FROM wallet_links").
+		WillReturnRows(sqlmock.NewRows([]string{"chain", "reporter", "user_id", "address", "alias"}))
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnRows(sqlmock.NewRows([]string{"name", "pretty_name", "base_denom", "bech32_validator_prefix"}))
+
+	mock.ExpectQuery("SELECT chain, name, proposal_link_pattern, wallet_link_pattern, validator_link_pattern, main_link FROM explorers").
+		WillReturnRows(sqlmock.NewRows([]string{"chain", "name", "proposal_link_pattern", "wallet_link_pattern", "validator_link_pattern", "main_link"}))
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		dataFetcher,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser", ID: 1},
+			Text:   "/wallets",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/wallets", ctx)
+	require.NoError(t, err)
+}
+
+//nolint:paralleltest // disabled
+func TestTelegramWalletsOk(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/getMe",
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-bot-ok.json")))
+
+	httpmock.RegisterMatcherResponder(
+		"POST",
+		"https://api.telegram.org/botxxx:yyy/sendMessage",
+		types.TelegramResponseHasBytes(assets.GetBytesOrPanic("responses/wallets.html")),
+		httpmock.NewBytesResponder(200, assets.GetBytesOrPanic("telegram-send-message-ok.json")))
+
+	logger := loggerPkg.GetDefaultLogger()
+	metricsManager := metrics.NewManager(logger, types.MetricsConfig{})
+	database := databasePkg.NewDatabase(logger, types.DatabaseConfig{})
+	dataFetcher := datafetcher.NewDataFetcher(logger, database, nil, metricsManager, nil)
+
+	db, mock, err := sqlmock.New()
+	require.NoError(t, err)
+
+	mock.ExpectExec("INSERT INTO queries").
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery("SELECT chain FROM chain_binds").
+		WillReturnRows(sqlmock.NewRows([]string{"chain"}))
+
+	mock.ExpectQuery("SELECT chain, reporter, user_id, address, alias FROM wallet_links").
+		WillReturnRows(sqlmock.NewRows([]string{"chain", "reporter", "user_id", "address", "alias"}).
+			AddRow("chain1", "telegram", 1, "address1", "alias1").
+			AddRow("chain2", "telegram", 1, "address1", "alias1"),
+		)
+
+	mock.ExpectQuery("SELECT name, pretty_name, base_denom, bech32_validator_prefix FROM chains").
+		WillReturnRows(sqlmock.
+			NewRows([]string{"name", "pretty_name", "base_denom", "bech32_validator_prefix"}).
+			AddRow("chain1", "Chain 1", "uatom", "cosmosvaloper").
+			AddRow("chain2", "Chain 2", "ustake", "cosmosvaloper"),
+		)
+
+	mock.ExpectQuery("SELECT chain, name, proposal_link_pattern, wallet_link_pattern, validator_link_pattern, main_link FROM explorers").
+		WillReturnRows(sqlmock.
+			NewRows([]string{"chain", "name", "proposal_link_pattern", "wallet_link_pattern", "validator_link_pattern", "main_link"}).
+			AddRow("chain1", "Ping", "", "https://example.com/wallets/%s", "", "").
+			AddRow("chain2", "Ping", "", "https://example2.com/wallets/%s", "", ""),
+		)
+
+	database.SetClient(db)
+
+	interacter := NewInteracter(
+		types.TelegramConfig{Token: "xxx:yyy", Admins: []int64{1, 2}},
+		"v1.2.3",
+		logger,
+		dataFetcher,
+		database,
+		metricsManager,
+	)
+	interacter.Init()
+
+	ctx := interacter.TelegramBot.NewContext(tele.Update{
+		ID: 1,
+		Message: &tele.Message{
+			Sender: &tele.User{Username: "testuser"},
+			Text:   "/wallets",
+			Chat:   &tele.Chat{ID: 2},
+		},
+	})
+
+	err = interacter.TelegramBot.Trigger("/wallets", ctx)
+	require.NoError(t, err)
+}
